842951b9b1bb67837d7b5d077f57910fbde107b6
strengur <steingrimur@740.is>
2016-01-17 12:56:31 +0000

    Styling of posts page
    Alignment and font-sizes for heading and paragraphs. Images unfinished

--- wp-content/themes/gluggalokanir/single.php
+++ wp-content/themes/gluggalokanir/single.php
@@ -1,3 +1,21 @@
 <?php include('header.php'); ?>
 
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<div class="page-content-clearfix">
+	<div class="post-content">
+		<h2><?php the_title(); ?></h2>
+		<?php the_content(); ?>
+	
+<?php endwhile; else: ?>
+
+		<p>Engar upplýsingar til að birta</p>
+
+<?php endif; ?>
+
+	</div>
+</div>
+
+
+
 <?php include('footer.php'); ?>
